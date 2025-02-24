50c833c85c6daf405cb68e7d91490447f3db620a
sambackhaus <sambackhaus@gmail.com>
2025-02-24 16:58:57 +0100

    use nested half adder in full adder

--- python/src/full_adder.py
+++ python/src/full_adder.py
@@ -2,7 +2,9 @@ from typing import List
 from adder import Adder
 from and_gate import AndGate
 from device import Device
+from half_adder import HalfAdder
 from or_gate import OrGate
+from static import Static
 from xor_gate import XorGate
 
 
@@ -17,17 +19,12 @@ class FullAdder(Adder):
             cin = []
         self.cin = cin
 
-        self.xor_gate_1 = XorGate(a=self.a, b=self.b)
-        self.and_gate_1 = AndGate(a=self.a, b=self.b)
-
-        self.xor_gate_2 = XorGate(a=[self.xor_gate_1], b=self.cin)
-        self.and_gate_2 = AndGate(a=[self.xor_gate_1], b=self.cin)
-
-        self.or_gate = OrGate(a=[self.and_gate_2], b=[self.and_gate_1])
+        self.half_adder_1 = HalfAdder(a=self.a, b=self.b)
+        self.half_adder_2 = HalfAdder(a=[self.half_adder_1], b=self.cin)
+        self.or_gate = OrGate(a=[Static(self.half_adder_1.get_cout())], b=[Static(self.half_adder_2.get_cout())])
 
     def get_state(self):
-        return self.xor_gate_2.get_state()
+        return self.half_adder_2.get_state()
     
     def get_cout(self):
         return self.or_gate.get_state()
-    
